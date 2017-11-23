d63d33bde6092241362886a6e04b03a7af295eb4
森田賢二 <kenjimoritata@gmail.com>
2017-11-23 21:35:04 +0900

    Add Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
+import {logo} from './logo.svg';
 import  { Home } from './container/Home';
 import  { About } from './container/About';
 import  { Topics } from './container/Topics';
 import './scss/utils/helper.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 
 function counter(state = 0, action) {
     switch (action.type) {
@@ -22,22 +23,21 @@ function counter(state = 0, action) {
 const store = createStore(counter);
 store.subscribe(()=> console.log(store.getState()));
 
-store.dispatch({type: 'INCREMENT'})
-store.dispatch({type: 'DECREMENT'})
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
 
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
+const Root = ({store}) => (
+    <Provider store={store}>
+        <Router>
+            <div>
             <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/topics' component={Topics} />
-        </div>
-      </Router>
-    );
-  }
-}
+                <Route exact path='/about' component={About} />
+                <Route exact path='/topics' component={Topics} />
+            </div>
+        </Router>
+    </Provider>
+)
 
-export default App;
+
+export default Root;
---
+++ src/reducers/reducers.js
@@ -0,0 +1,3 @@
+
+
+export const ohmajimeStore = () =>( {value: "fafa"});
