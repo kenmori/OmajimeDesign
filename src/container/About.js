a0344b4557777f8e1614a7d3ee3d0e22d1a8aae3
森田賢二 <kenjimoritata@gmail.com>
2018-01-26 18:39:21 +0900

    add yarn flow

--- src/container/About.js
+++ src/container/About.js
@@ -1,6 +1,7 @@
+//@flow
 import React, { Component } from 'react';
 
-export class About extends Component {
+export class About extends Component <{}> {
     render(){
         return (
             <div>About</div>
