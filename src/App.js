37af2f889da793db6c3d1c358e7b4b5651234810
森田賢二 <kenjimoritata@gmail.com>
2018-02-17 19:31:37 +0900

    logger

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import { About } from './container/About';
 import { Topics } from './container/Topics';
 import './scss/utils/helper.css';
 import { Provider } from 'react-redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { BrowserRouter as  Router, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
-import { browseerHistory } from 'react-router';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers/reducers';
 import { rootSaga } from './sagas/sagas';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-
+import browserHistory from 'react-router/lib/browserHistory';
 
 const defaultState = {};
-const sagaMiddlware = createSagaMiddleware();
-let store = createStore(
+const middlewares = []
+if(process.env.NODE_ENV === 'development'){
+    const {logger} = require('redux-logger')
+    middlewares.push(logger);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+middlewares.push(sagaMiddleware)
+const store = createStore(
     reducers,
     defaultState,
-    composeWithDevTools(applyMiddleware(sagaMiddlware))
+    composeWithDevTools(applyMiddleware(...middlewares))
 );
 
-sagaMiddlware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 const App = () => (
     <Provider store={store}>
-    <Router>
+    <Router history={history}>
         <div>
          <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,7 +4,7 @@ import {reducer as formReducer} from 'redux-form';
 import {topickReducer} from './TopickReducer';
 
 const reducers =  combineReducers({
-   routing: routerReducer,
+    routing: routerReducer,
     form: formReducer,
     topick: topickReducer,
 });
